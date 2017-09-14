5da7731aa2dbfc7770ff9fa8583f49c660411180
Lien Phan <whyofcourse@gmail.com>
2017-09-14 23:06:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,16 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
 
-function init(e) {
-  const key = parseInt(e.detail || e.which)
-  if(key === code[index]){
-    index++
-    if(index === code.length){
-      alert("Congrats!")
+function init() {
+  document.body.addEventListener('keydown', function (e){
+    const key = parseInt(e.detail || e.which)
+    if(key === code[index]){
+      index++
+      if(index === code.length){
+        alert("Congrats!")
+        index = 0
+      }
+    }else{
+      alert("Wrong!")
       index = 0
     }
-  }else{
-    alert("Wrong!")
-    index = 0
-  }
+  })
 }
